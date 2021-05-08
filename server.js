474cf605c9184c36847daca3108825751d8ae8b4
Santos <max.go.95@gmail.com>
2021-05-08 13:52:05 -0500

    basic routes

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
-var PORT = process.env.PORT || 3001;
+const PORT = 3000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// array of table objects
+tables = [
+    {
+
+    }, 
+    {
+
+    }, 
+    {
+
+    }, 
+    {
+
+    }, 
+
+    {
+
+    }
+];
+
+
+// waitlist array
+waitlist = [
+    {
+
+    }
+];
+
+// regular html get requests
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')))
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')))
+
+
+// return json 
+app.get('/api/tables', (req, res) => res.json(tables));
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
 
 
 
-
+app.listen(PORT, ()=> console.log(`App is listening on ${PORT}`))
 
 
 
