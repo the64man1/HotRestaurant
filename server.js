e786d333436fc2c111d8d8521f48f1bb81979b59
Cristobal Canales <Cristobal.Canales@gmail.com>
2021-05-08 13:28:25 -0500

    Update server.js file

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const reservations = [];
 
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get("/tables", (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+app.get("/tables", (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 app.get("/reserve", (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
