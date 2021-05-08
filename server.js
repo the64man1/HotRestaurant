14a6ac1d5b441ef114fc8d4b8dacf9a0e42e8beb
MohammedOttun <ottunmo@mac.com>
2021-05-08 12:45:52 -0500

    Added express dependency and listening port.

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+var PORT = process.env.PORT || 3001;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
